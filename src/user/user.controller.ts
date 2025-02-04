21d626adc33fda04d8cfee41cc2380b0f66c77d1
SrLuc <ruanlucenaramos@gmail.com>
2025-02-04 19:19:33 -0300

    feat: adding crud operations to user routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get, Req, UseGuards } from "@nestjs/common";
+import { Controller, Delete, Get, Put, Req, UseGuards } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { AuthGuard } from "@nestjs/passport";
 
 @Controller("users")
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @UseGuards(AuthGuard("jwt"))
   @Get("me")
   getMe(@Req() req) {
     return req.user;
+  }
+
+  @Get()
+  async getAll() {
+    return this.userService.getAll();
+  }
+
+  @Get()
+  async getOne(@Req() req) {
+    return this.userService.getOne(req.user.id);
+  }
+
+  @Put()
+  async update(@Req() req) {
+    return this.userService.update(req.user.id);
+  }
+
+  @Delete()
+  async delete(@Req() req) {
+    return this.userService.delete(req.user.id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,5 +4,21 @@ import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class UserService {
- 
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAll() {
+    return this.prisma.user.findMany();
+  }
+
+  async getOne(id: number) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async update(id: number) {
+    return this.prisma.user.update({ where: { id }, data: {} });
+  }
+
+  async delete(id: number) {
+    return this.prisma.user.delete({ where: { id } });
+  }
 }
